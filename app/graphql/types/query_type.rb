6a4bbad5c985cf5539c97888e0223904065bfdbc
ayrton <ayrton.parkinson1@gmail.com>
2022-07-14 07:02:26 -0400

    Add LinksSearch resolver to allLinks query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,7 @@
 module Types
   class QueryType < Types::BaseObject
     # Queries represented as fields
-    field :all_links, [LinkType], null: false
+    field :all_links, resolver: Resolvers::LinksSearch
 
     # Invoked when 'all_link' fields resolved
     def all_links
